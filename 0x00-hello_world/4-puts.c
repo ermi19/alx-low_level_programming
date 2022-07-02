948d38b6f2cd2b8b0db1467e78cb11d996cb95c8
ermi19 <ermiyasgera@gmail.com>
2022-07-02 02:16:39 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
-#include <stdio.h>
+#include<stdio.h>
 
-/*
- *main - entry point,
+/**
+ *main - Entry point
  *
- *Return:  0
- */
+ *Return: Always 0 (Success)
+ **/
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }
