5e07df46605721864dc4dfc61a3cbf072c4338f4
ermi19 <ermiyasgera@gmail.com>
2022-07-06 12:53:24 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *print_alphabet - function to print abc
- *
- *Return: 0
- */
+*print_alphabet - function to print abc
+*
+*Return: 0
+*/
 void print_alphabet(void)
 {
 	char c;
@@ -12,5 +12,5 @@ void print_alphabet(void)
 	{
 		_putchar(c);
 	}
-_putchar('\n');
+	_putchar('\n');
 }
