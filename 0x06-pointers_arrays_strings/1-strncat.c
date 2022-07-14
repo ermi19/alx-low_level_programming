8db093c22061ccb7ee709f88c8361804492ac0a7
ermi19 <ermiyasgera@gmail.com>
2022-07-14 10:28:40 -0700

    all

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+/**
+ *_strncat - appends first n characters from src to dest
+ *
+ *@dest: destination array
+ *@src: source array
+ *@n: number of characters
+ *
+ *Return: dest string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *ptr = dest;
+
+	while (*ptr != 0)
+		ptr++;
+	while (n-- > 0)
+		*ptr++ = *src++;
+	*ptr = 0;
+
+	return (dest);
+}
