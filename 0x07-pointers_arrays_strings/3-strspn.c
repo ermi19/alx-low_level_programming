9e3621a5ccb12a94c2d215f09b60c2badeaec326
ermi19 <ermiyasgera@gmail.com>
2022-07-19 13:02:50 -0700

    all

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,17 +1,17 @@
 /**
- *_strspn - Returns the number of bytes in the initial segment
- *of s which consist only of bytes from accept
+ * _strspn - Returns the number of bytes in the initial segment
+ * of s which consist only of bytes from accept
  *
- *@s: string to search
- *@accept: characters to look for
+ * @s: string to search
+ * @accept: characters to look for
  *
- *Return: number of bytes that matched at start of string
+ * Return: number of bytes that matched at start of string
  */
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int sum = 0;
 	char *ptr = accept;
-	
+
 	while (*s != 0)
 	{
 		if (*s == *ptr && *ptr != 0)
